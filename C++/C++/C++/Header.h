fdb2f864b4b9b96a36d4be221a59cb257f9c8cb1
Richmond <Richlaureta@gmail.com>
2025-06-06 23:20:11 -0700

    Added hasCycle in a linked list problem.

--- C++/C++/C++/Header.h
+++ C++/C++/C++/Header.h
@@ -16,3 +16,24 @@
 bool isAnagram(std::string s, std::string t);
 
 double findMaxAverage(std::vector<int> &nums, int k);
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    
+    ListNode(int x) : val(x), next(NULL){}
+};
+
+class LinkedList
+{
+private:
+    ListNode *Head = NULL;
+    
+public:
+    void insertNode(ListNode *node);
+    
+    void printLinkedList();
+};
+
+bool hasCycle(ListNode *head);
--- C++/C++/C++/main.cpp
+++ C++/C++/C++/main.cpp
@@ -14,6 +14,7 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
+    //Anagram Problem
     string s = "anagram";
     string t = "nagaram";
     
@@ -28,10 +29,36 @@ int main(int argc, const char * argv[]) {
         cout << "not Anagrams!" << endl;
     }
     
+    //Maximum Subarray Average I Problem
     vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int k = 4;
     
     cout << findMaxAverage(arr, k) << endl;
+    
+    //Linked List Cycle Problem
+    ListNode node0 = ListNode(3);
+    ListNode node1 = ListNode(2);
+    ListNode node2 = ListNode(0);
+    ListNode node3 = ListNode(-4);
+    
+    LinkedList linkedList0;
+    
+    linkedList0.insertNode(&node0);
+    linkedList0.insertNode(&node1);
+    linkedList0.insertNode(&node2);
+    linkedList0.insertNode(&node3);
+//    linkedList0.insertNode(&node1);
+    
+    if(hasCycle(&node0) == 1)
+    {
+        std::cout << "The linked list has a cycle." << std::endl;
+    }
+    else
+    {
+        std::cout << "The linked list is not a cycle." << std::endl;
+    }
+    
+//    linkedList0.printLinkedList();
     
     return EXIT_SUCCESS;
 }
