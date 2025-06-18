d98a158728f08be2e5a5afdfff58039d0b964dc2
Richmond <Richlaureta@gmail.com>
2025-06-18 12:32:29 -0700

    Added solution to Problem #56 Merge Intervals in Python.

---
+++ Python/OverlappingIntervals.py
@@ -0,0 +1,30 @@
+import sys
+
+def merge(intervals: list[list[int]]) -> list[list[int]]:
+    if len(intervals) == 1 or len(intervals) == 0 :
+        return intervals
+    
+    intervals.sort(key = lambda time: time[0])
+    merged = [intervals[0]]
+    index = 1
+ 
+    while(index != len(intervals)):
+        if intervals[index][0] <= merged[len(merged) - 1][1] and intervals[index][1] > merged[len(merged) - 1][1]:
+            merged[len(merged) - 1][1] = intervals[index][1]
+        elif intervals[index][0] <= merged[len(merged) - 1][1] and intervals[index][1] <= merged[len(merged) - 1][1]:
+            index += 1
+            continue
+        elif intervals[index][0] > merged[len(merged) - 1][1]:
+            merged.append(intervals[index])
+        index += 1
+
+    return merged
+
+def main():
+    # intervals = [[1,3],[0,2],[2,3],[4,6],[4,5],[5,5],[0,2],[3,3]]
+    # intervals = [[1,3],[2,6],[8,10],[15,18]]
+    intervals = [[1, 4], [0, 1]]
+    print(merge(intervals))
+
+if __name__ == "__main__":
+    sys.exit(main())
