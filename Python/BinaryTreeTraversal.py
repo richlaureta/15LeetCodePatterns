d84702a6a208fb01e23f3a946dece376d9c4c354
Richmond <Richlaureta@gmail.com>
2025-06-22 22:12:43 -0700

    Added a solution to Problem #102. Binary Tree Level Order Traversal.

--- Python/BinaryTreeTraversal.py
+++ Python/BinaryTreeTraversal.py
@@ -1,19 +1,20 @@
 import sys
+from collections import deque
 
-class Node:
-    def __init__(self, value: int):
+class TreeNode:
+    def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
     
-def preOrder(node: Node):
+def preOrder(node: TreeNode):
         if node is None:
              return
         print(node.value, end=" ")
         preOrder(node.left)
         preOrder(node.right)
 
-def inOrder(node: Node):
+def inOrder(node: TreeNode):
         if node is None:
              return
         inOrder(node.left)
@@ -21,25 +22,45 @@ def inOrder(node: Node):
         inOrder(node.right)
         
 
-def postOrder(node: Node):
+def postOrder(node: TreeNode):
         if node is None:
              return
+
         postOrder(node.left)
         postOrder(node.right)
         print(node.value, end=" ")
 
+def levelOrder(root: TreeNode) -> list[list[int]]:
+    #Problem #102. Binary Tree Level Order Traversal - I learned from a youtuber Greg Hogg
 
+    if root is None:
+        return
     
+    q = deque()
+    q.append(root)
+    ans = []
+
+    while q:
+        count += 1
+        level = []
+        for i in range(len(q)):
+            node = q.popleft()
+            level.append(node.value)
+            if node.left: q.append(node.left)
+            if node.right: q.append(node.right)
+        ans.append(level)
+        
+    return ans
 
 def main():
     # Create a sample binary tree
-    root = Node(1)
-    root.left = Node(2)
-    root.right = Node(3)
-    root.left.left = Node(4)
-    root.left.right = Node(5)
-    root.right.left = Node(6)
-    root.right.right = Node(7)
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(7)
 
     preOrder(root)
     print()
@@ -47,6 +68,7 @@ def main():
     print()
     postOrder(root)
     print()
+    print(levelOrder(root))
     
 if __name__ == "__main__":
     sys.exit(main())
