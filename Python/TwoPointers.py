76b8db6ad33e168dc355b63d70c8945dac68bd8e
Richmond <Richlaureta@gmail.com>
2025-08-07 15:12:48 -0700

    Added solution to Problem #15 3Sum in Python with the help of Google Gemini with debugging.

--- Python/TwoPointers.py
+++ Python/TwoPointers.py
@@ -1,4 +1,40 @@
-def twoSum(numbers: list[int], target: int) -> list[int]:
+def threeSum(nums: list[int]) -> list[list[int]]:
+    nums.sort()
+    print(nums)
+    answer = []
+    leftIndex = 0
+    rightIndex = 0
+    sum = 0
+    
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i-1]: #Help of Google CLI AI - debugged snippet code
+            continue
+
+        leftIndex = i + 1
+        rightIndex = len(nums) - 1
+        
+        while leftIndex < rightIndex:
+            sum = nums[i] +  nums[leftIndex] + nums[rightIndex]
+
+            if sum == 0:
+                answer.append([nums[i], nums[leftIndex], nums[rightIndex]])
+                
+
+                rightIndex -= 1
+                while(leftIndex < rightIndex and nums[rightIndex] == nums[rightIndex + 1]):
+                    rightIndex -= 1
+
+                leftIndex += 1
+                while(leftIndex < rightIndex and nums[leftIndex] == nums[leftIndex - 1]):
+                    leftIndex += 1
+                
+            elif sum > 0:
+                rightIndex -= 1
+            else:
+                leftIndex += 1
+    return answer
+
+def twoSum2(numbers: list[int], target: int) -> list[int]:
     startIndex = 0
     endIndex = len(numbers) - 1
 
@@ -11,7 +47,11 @@ def twoSum(numbers: list[int], target: int) -> list[int]:
             startIndex += 1
 
 if __name__ == "__main__":
-    numbers = [2, 7, 11, 15]
-    target = 9
+    # numbers = [2, 7, 11, 15]
+    # target = 9
 
-    print(twoSum(numbers, target))
+    # print(twoSum2(numbers, target))
+
+    nums = [-1, 0, 1, 2, -1, -1, -4]
+
+    print(threeSum(nums))
