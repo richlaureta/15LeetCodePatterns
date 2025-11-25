bce870b1cc6050bffeee70ac114870505c640015
Richmond <Richlaureta@gmail.com>
2025-11-25 11:35:45 -0800

    Recursion Practice 06.

--- Python/RecursionPractice.py
+++ Python/RecursionPractice.py
@@ -18,35 +18,34 @@ def recursiveLucasSequence(previousValue, sequence, howManySequence, arrayList: 
     
     recursiveLucasSequence(previousValue + 1, sequence + 1, howManySequence, arrayList, iterationCount + 1)
 
-def recursiveFibonacciSequence(howManySequence: int) -> list[int]:
+def recursiveFibonacciSequence(index: int) -> int:
     sequenceList: list[int] = []
     
-    def depthFirstSearchFibonacci(previousValue1: int, previousValue2: int, howManySequence: int, iterationCount: int = 0):        
-        if iterationCount == howManySequence:
+    def depthFirstSearchFibonacci(previousValue1: int, previousValue2: int, index: int, iterationCount: int = -1):        
+        if iterationCount == index:
             return
         
-        if iterationCount > 1:
+        if iterationCount > 0:
             sequenceList.append(previousValue1 + previousValue2)
             temporaryValue = previousValue1
             previousValue1 = previousValue1 + previousValue2
             previousValue2 = temporaryValue
         else:
-            if iterationCount == 0:
+            if iterationCount == -1:
                 sequenceList.append(0)
             else:
                 sequenceList.append(1)
                 previousValue1 = 1
                 previousValue2 = 0
                   
-        depthFirstSearchFibonacci(previousValue1, previousValue2, howManySequence, iterationCount + 1)
+        depthFirstSearchFibonacci(previousValue1, previousValue2, index, iterationCount + 1)
         
-    depthFirstSearchFibonacci(None, None, howManySequence)
+    depthFirstSearchFibonacci(None, None, index)
     
-    return sequenceList
+    return sequenceList[index]
 
 if __name__ == "__main__":
-    howManySequence: int = int(input("How many sequence of numbers do you want to see in the Fibonacci pattern(s)? "))
     
-    print(recursiveFibonacciSequence(howManySequence))
+    print(recursiveFibonacciSequence(6))
     
     
