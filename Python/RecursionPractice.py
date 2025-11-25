e58a724e8743d82dd03524912229e88f6306fe15
Richmond <Richlaureta@gmail.com>
2025-11-24 20:06:05 -0800

    Practicing Recursion for Today 00.

---
+++ Python/RecursionPractice.py
@@ -0,0 +1,15 @@
+def recursiveAddition(count: int, endCount: int):
+    if count == endCount:
+        return
+    
+    print(count)	
+    recursiveAddition(count + 1, endCount)
+
+def recursiveSequence(initialValue: int, endCount)
+if __name__ == "__main__":
+   initialValue = 2
+   endCount = 8
+   
+   recursiveAddition(count, endCount)   
+   
+   
