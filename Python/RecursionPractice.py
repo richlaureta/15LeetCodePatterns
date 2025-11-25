2bdc6cddd6fa3bd4e43750e9df60eba4fde90ef4
Richmond <Richlaureta@gmail.com>
2025-11-24 22:09:09 -0800

    Recursion Practice 03.

--- Python/RecursionPractice.py
+++ Python/RecursionPractice.py
@@ -5,21 +5,23 @@ def recursiveAddition(count: int, endCount: int):
     print(count)	
     recursiveAddition(count + 1, endCount)
 
-def recursiveLucasSequence(previousValue, sequence, upToIndex, iterationCount = 0):
-    if iterationCount == upToIndex:
+def recursiveLucasSequence(previousValue, sequence, howManySequence, arrayList: list[int], iterationCount = 0):
+    if iterationCount == howManySequence:
         return
     
     if iterationCount == 0:
-        print(previousValue)
+        arrayList.append(previousValue)
         sequence = 1
     else:
-        print(previousValue + sequence)
+        arrayList.append(previousValue + sequence)
         previousValue = previousValue + sequence
     
-    recursiveLucasSequence(previousValue + 1, sequence + 1, upToIndex, iterationCount + 1)
+    recursiveLucasSequence(previousValue + 1, sequence + 1, howManySequence, arrayList, iterationCount + 1)
 
 if __name__ == "__main__":
 
-    howManySequence = 5
+    howManySequence = 10
+    array = []
+    recursiveLucasSequence(2, 0, howManySequence, array)
     
-    recursiveLucasSequence(2, 0, howManySequence)
+    print(array)
