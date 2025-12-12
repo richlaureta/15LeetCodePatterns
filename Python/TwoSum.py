9da29b83e87b5c24002eaec6ae7634c329aae276
Richmond <Richlaureta@gmail.com>
2025-12-11 23:23:18 -0800

    Added solution for Problem #1 Two Sum in Python.

--- Python/TwoSum.py
+++ Python/TwoSum.py
@@ -1,15 +1,13 @@
 def twoSum(nums: list[int], target: int) -> list[int]:
-    #Problem #1 Two Sum
+    #Problem #1 Two Sum - Easy
+    numberDictionary = {}
     
-    myDictionary = {}
-
-    for i in range(0, len(nums)):
-        difference = target - nums[i]
-
-        if difference in myDictionary and i != myDictionary[difference]:
-            return [myDictionary[difference], i]
-         
-        myDictionary[nums[i]] = i
+    for index, number in enumerate(nums):
+        difference = target - number
+        if difference in numberDictionary:
+            return [numberDictionary[difference], index]
+        
+        numberDictionary[number] = index
 
 if __name__ == "__main__":
     nums = [2, 7, 11, 15]
