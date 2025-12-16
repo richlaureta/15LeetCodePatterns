8b5796423fa982c1844882432e1d921f827c4f4b
Richmond <Richlaureta@gmail.com>
2025-12-15 21:24:26 -0800

    Added comments and explanation on Problem #525 Contiguous Array in C++.

--- C++/C++/PrefixSum.cpp
+++ C++/C++/PrefixSum.cpp
@@ -10,31 +10,41 @@
 int findMaxLength(vector<int> &nums)
 {
     //Problem #525 Contiguous Array - Medium - Solution Concept by YouTube Channel Cracking FAANG - Understanding the Solution
-
-    int countOffset = 0;
-    unordered_map<int, int> offsetMap = {{0, -1}};
     
+    //Initialize the offset variable. We will add 1 if it is a 1 and will subtract 1 if it is a zero.
+    int countOffset = 0;
+    //Initialize an unordered map to store the offset value paired with the index.
+    //We will start with the imaginary index -1 to store the 0 count offset.
+    unordered_map<int, int> offsetMap = {{0, -1}};
+    //Initialize a variable to count the maximum length of equal 0’s and 1’s.
     int maxLength = 0;
     
+    //Iterate through the nums.
     for(int i = 0; i < nums.size(); i++)
     {
+        //If the nums[i] is 1, add 1 to countOffset.
         if(nums[i] == 0) countOffset--;
-    
+        //If nums[i] is 0, subtract 1 from countOffset.
         else countOffset++;
         
+        //If countOffset becomes 0, let maxLength equal the index + 1.
         if(countOffset == 0)
         {
             maxLength = i + 1;
         }
+        //If countOffset is in the dictionary, get the maximum between maxLength and index minus
+        //the offsetDictionary[countOffset], and continue.
         else if(offsetMap.find(countOffset) != offsetMap.end())
         {
             maxLength = max(maxLength, i - offsetMap[countOffset]);
             continue;
         }
         
+        //Store the countOffset as the key and let that equal the index.
         offsetMap[countOffset] = i;
     }
     
+    //Return the maxLength.
     return maxLength;
 }
 
