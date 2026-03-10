81dad0e4dcfde1a68da2701dc046313ea49cb7fe
Richmond <Richlaureta@gmail.com>
2026-03-09 20:44:34 -0700

    Revisited solution to Problem #56 Merge Intervals in Python - not the most efficient - 01.

--- Python/OverlappingIntervals.py
+++ Python/OverlappingIntervals.py
@@ -6,18 +6,17 @@ def merge(intervals: list[list[int]]) -> list[list[int]]:
     for index in range(len(intervals) - 1):
         previous = [newMergedIntervalsList[len(newMergedIntervalsList) - 1][0], newMergedIntervalsList[len(newMergedIntervalsList) - 1][1]]
         
-        if previous[1] >= intervals[index + 1][0] and intervals[index + 1][1] >= previous[1]:
-            newMergedIntervalsList.pop()
-            newMergedIntervalsList.append([previous[0], intervals[index + 1][1]])
-            
-        elif previous[1] >= intervals[index + 1][0] and previous[1] >= intervals[index + 1][1]:
-            newMergedIntervalsList.pop()
-            newMergedIntervalsList.append([previous[0], previous[1]])
+        if previous[1] >= intervals[index + 1][0]:
+            if intervals[index + 1][1] < previous[1]:
+                newMergedIntervalsList.pop()
+                newMergedIntervalsList.append([previous[0], previous[1]])
+            else:
+                newMergedIntervalsList.pop()
+                newMergedIntervalsList.append([previous[0], intervals[index + 1][1]])
         else:
             newMergedIntervalsList.append([intervals[index + 1][0], intervals[index + 1][1]])
         
     return newMergedIntervalsList
-
 
 def insert(intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
     #Problem #57 Insert Interval - Medium
