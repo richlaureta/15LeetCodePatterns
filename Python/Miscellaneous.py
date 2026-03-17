791616309c7c5978e88e35f08431dd82c358e75b
Richmond <Richlaureta@gmail.com>
2026-03-16 22:31:21 -0700

    Saving incomplete solution.

--- Python/Miscellaneous.py
+++ Python/Miscellaneous.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 def checkOnesSegment(s: str) -> bool:
     #Problem #1784 Check if Binary String Has at Most One Segment of Ones
     
@@ -16,6 +17,31 @@ def checkOnesSegment(s: str) -> bool:
 	
 	return True
 
+def findAndReplacePattern(words: list[str], pattern: str) -> list[str]:
+    #Problem #890 Find and Replace Pattern - Medium
+    
+    patternFrequencyDictionary = defaultdict(int)
+    patternSet = set()
+    orderOfLetter = []
+    
+    for letter in pattern:
+        if letter not in patternSet:
+            orderOfLetter.append(letter)
+            patternSet.add(letter)
+        patternFrequencyDictionary[letter] += 1
+    
+    wordsPatternMatch = []
+    
+    for word in words:
+        letterFrequencyDictionary = defaultdict(int)
+        wordPatternSet = set()
+        orderOfLetter1 = []
+        for letter1 in word:
+            pass    
+    
+    return wordsPatternMatch
 if __name__ == "__main__":
-    s = "0011"
-    print(checkOnesSegment(s))
+    words = ["abc","deq","mee","aqq","dkd","ccc"]
+    pattern = "abb"
+    
+    print(findAndReplacePattern(words, pattern))
