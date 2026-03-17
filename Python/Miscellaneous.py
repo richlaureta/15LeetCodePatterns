7480e71ec446db1e8a9bb0dc475eb9674c1c4046
Richmond <Richlaureta@gmail.com>
2026-03-17 01:15:35 -0700

    Another approach to solving Problem #890 Find and Replace Pattern in Python.

--- Python/Miscellaneous.py
+++ Python/Miscellaneous.py
@@ -21,37 +21,38 @@ def findAndReplacePattern(words: list[str], pattern: str) -> list[str]:
     #Problem #890 Find and Replace Pattern - Medium
     
     patternFrequencyDictionary = defaultdict(list[int])
+    letterToUniqueCount = defaultdict(int)
+    uniqueCount = 1
     patternSet = set()
-    orderOfLetter = []
     
     for index0, letter in enumerate(pattern):
         if letter not in patternSet:
-            orderOfLetter.append(letter)
             patternSet.add(letter)
-        patternFrequencyDictionary[letter].append(index0)
+            letterToUniqueCount[letter] = uniqueCount
+            patternFrequencyDictionary[letterToUniqueCount[letter]].append(index0)
+            uniqueCount += 1
+        else:
+            patternFrequencyDictionary[letterToUniqueCount[letter]].append(index0)
     
     wordsPatternMatch = []
     
     for word in words:
         letterFrequencyDictionary = defaultdict(list[int])
         wordPatternSet = set()
-        orderOfLetter1 = []
+        letterToUniqueCount1 = defaultdict(int)
+        uniqueCount1 = 1
         for index1, letter1 in enumerate(word):
             if letter1 not in wordPatternSet:
-                orderOfLetter1.append(letter1)
                 wordPatternSet.add(letter1)
-            letterFrequencyDictionary[letter1].append(index1)
+                letterToUniqueCount1[letter1] = uniqueCount1
+                letterFrequencyDictionary[letterToUniqueCount1[letter1]].append(index1)
+                uniqueCount1 += 1
+            else:
+                letterFrequencyDictionary[letterToUniqueCount1[letter1]].append(index1)
         
-        if len(orderOfLetter) != len(orderOfLetter1):
-            continue
-        
-        for index2, letter2 in enumerate(orderOfLetter):
-            if patternFrequencyDictionary[letter2] != letterFrequencyDictionary[orderOfLetter1[index2]]:
-                break
-        
-        if len(orderOfLetter) - 1 == index2:
-            wordsPatternMatch.append(word)  
-    
+        if patternFrequencyDictionary == letterFrequencyDictionary:
+            wordsPatternMatch.append(word)
+            
     return wordsPatternMatch
 
 if __name__ == "__main__":
