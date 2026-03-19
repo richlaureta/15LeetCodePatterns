22987f09cf1e1c1ced42968f7f3a95ec57d0c88a
Richmond <Richlaureta@gmail.com>
2026-03-18 19:35:03 -0700

    Revisited and solved Problem #240 Search a 2D Matrix II using brute force in Python - not the most efficient solution.

--- Python/ModifiedBinarySearch.py
+++ Python/ModifiedBinarySearch.py
@@ -58,20 +58,15 @@ def findMin(nums: list[int]) -> int:
 def searchMatrix(matrix: list[list[int]], target: int) -> bool:
     #Problem # 240 Search a 2D Matrix II - Medium
     
-    row = len(matrix) - 1
-    column = 0 
+    for row in matrix:
+        for number in row:
+            if number == target:
+                return True
 
-    while row > -1  and column < len(matrix[0]):
-        if matrix[row][column] == target:
-            return True
-            
-        if matrix[row][column] > target:
-            row -= 1
-        else: column += 1
-        
     return False
 
 if __name__ == "__main__":
-    nums = [3, 1, 2]
+    nums = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+    target = 5
      
-    print(findMin(nums))
+    print(searchMatrix(nums, target))
