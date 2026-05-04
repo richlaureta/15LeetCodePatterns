34ee491430c1a4e7bff6af9ced3e1a569e4b06f2
Richmond <Richlaureta@gmail.com>
2026-05-04 15:21:36 -0700

    Concept from a submitted code in leetcode for Problem #36 Valid Sudoku in Python - more efficient.

--- Python/Miscellaneous.py
+++ Python/Miscellaneous.py
@@ -62,111 +62,23 @@ def findAndReplacePattern(words: list[str], pattern: str) -> list[str]:
 def isValidSudoku(board: list[list[str]]) -> bool:
     #Problem #36 Valid Sudoku - Medium
     
-    topLeftSet = set()
-    topMiddleSet = set()
-    topRightSet = set()
+    rowDictionary = defaultdict(set)
+    columnDictionary = defaultdict(set)
+    subGridDictionary = defaultdict(set)
     
-    middleLeftSet = set()
-    middleMiddleSet = set()
-    middleRightSet = set()
-    
-    bottomLeftSet = set()
-    bottomMiddleSet = set()
-    bottomRightSet = set()
-    
-    column0Set = set()
-    column1Set = set()
-    column2Set = set()
-    column3Set = set()
-    column4Set = set()
-    column5Set = set()
-    column6Set = set()
-    column7Set = set()
-    column8Set = set()
-    
-    for index in range(len(board)):
-        numberSeenRow = set()
-        for index1 in range(len(board[0])):
-            if board[index][index1] != "." and board[index][index1] in  numberSeenRow:
+    for row in range(9):
+        for column in range(9):
+            if board[row][column] == ".":
+                continue
+            elif (board[row][column] in rowDictionary[row] or 
+                  board[row][column] in columnDictionary[column] or 
+                  board[row][column] in subGridDictionary[(row//3, column//3)]):
                 return False
             else:
-                numberSeenRow.add(board[index][index1])
-            
-            match index1:
-                case 0:
-                    if board[index][index1] != "." and board[index][index1] in column0Set:
-                        return False
-                    column0Set.add(board[index][index1])
-                case 1:
-                    if board[index][index1] != "." and board[index][index1] in column1Set:
-                        return False
-                    column1Set.add(board[index][index1])
-                case 2:
-                    if board[index][index1] != "." and board[index][index1] in column2Set:
-                        return False
-                    column2Set.add(board[index][index1])
-                case 3:
-                    if board[index][index1] != "." and board[index][index1] in column3Set:
-                        return False
-                    column3Set.add(board[index][index1])
-                case 4:
-                    if board[index][index1] != "." and board[index][index1] in column4Set:
-                        return False
-                    column4Set.add(board[index][index1])
-                case 5:
-                    if board[index][index1] != "." and board[index][index1] in column5Set:
-                        return False
-                    column5Set.add(board[index][index1])
-                case 6:
-                    if board[index][index1] != "." and board[index][index1] in column6Set:
-                        return False
-                    column6Set.add(board[index][index1])
-                case 7:
-                    if board[index][index1] != "." and board[index][index1] in column7Set:
-                        return False
-                    column7Set.add(board[index][index1])
-                case 8:
-                    if board[index][index1] != "." and board[index][index1] in column8Set:
-                        return False
-                    column8Set.add(board[index][index1])
-            
-            if index < 3 and index1 < 3:
-                if board[index][index1] != "." and board[index][index1] in topLeftSet:
-                    return False
-                topLeftSet.add(board[index][index1])
-            elif index < 3 and index1 > 2 and index1 < 6:
-                if board[index][index1] != "." and board[index][index1] in topMiddleSet:
-                    return False
-                topMiddleSet.add(board[index][index1])
-            elif index < 3 and index1 > 5:
-                if board[index][index1] != "." and board[index][index1] in topRightSet:
-                    return False
-                topRightSet.add(board[index][index1])
-            elif index > 2 and index < 6 and index1 < 3 and index1 < 3:
-                if board[index][index1] != "." and board[index][index1] in middleLeftSet:
-                    return False
-                middleLeftSet.add(board[index][index1])
-            elif index > 2 and index < 6 and index1 > 2 and index1 < 6:
-                if board[index][index1] != "." and board[index][index1] in middleMiddleSet:
-                    return False
-                middleMiddleSet.add(board[index][index1])
-            elif index > 2 and index < 6 and index1 > 5:
-                if board[index][index1] != "." and board[index][index1] in middleRightSet:
-                    return False
-                middleRightSet.add(board[index][index1])
-            elif index > 5 and index1 < 3:
-                if board[index][index1] != "." and board[index][index1] in bottomLeftSet:
-                    return False
-                bottomLeftSet.add(board[index][index1])
-            elif index > 5 and index1 > 2 and index1 < 6:
-                if board[index][index1] != "." and board[index][index1] in bottomMiddleSet:
-                    return False
-                bottomMiddleSet.add(board[index][index1])
-            elif index > 5 and index1 > 5:
-                if board[index][index1] != "." and board[index][index1] in bottomRightSet:
-                    return False
-                bottomRightSet.add(board[index][index1])
-    
+                rowDictionary[row].add(board[row][column])
+                columnDictionary[column].add(board[row][column])
+                subGridDictionary[(row//3, column//3)].add(board[row][column])
+                
     return True
 
 if __name__ == "__main__":
